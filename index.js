276a7ff09832b5df35adf64d5828ff2be5b77c62
ericknovoa <ericknovoa8@gmail.com>
2026-02-19 12:28:09 -0500

    ingresamos las opciones de las operaciones

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
 console.log ("Hola, esta es mi sistema matematico de operaciones basicas: ");
+var opcion = prompt(ingrese su opcion: 1 para suma, 2 para resta, 3 para multiplicacion, 4 para division);
