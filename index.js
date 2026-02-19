6d64123ed0d8acfe431ff2a59cf0c8d997d4df67
ericknovoa <ericknovoa8@gmail.com>
2026-02-19 12:36:10 -0500

    ingresamos las opciones por switch para las peraciones de suma y resta

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ var opcion = prompt(ingrese su opcion: 1 para suma, 2 para resta, 3 para multipl
 
 var num1 = parseFloat(prompt("Ingrese el primer numero: "));
 var num2 = parseFloat(prompt("Ingrese el segundo numero: "));
+
+switch (opcion) {
+    case "1":
+        var resultado = num1 + num2;
+        console.log("El resultado de la suma es: " + resultado);
+        break;
+    case "2":
+        var resultado = num1 - num2;
+        console.log("El resultado de la resta es: " + resultado);
+        break;
