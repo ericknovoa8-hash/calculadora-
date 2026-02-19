cb670c44c76cab4fff13acc03a9953f5580f0b1b
ericknovoa <ericknovoa8@gmail.com>
2026-02-19 12:32:14 -0500

    Agregamos las opciones para ingresar los numeros y poder hacer las operaciones

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 console.log ("Hola, esta es mi sistema matematico de operaciones basicas: ");
 var opcion = prompt(ingrese su opcion: 1 para suma, 2 para resta, 3 para multiplicacion, 4 para division);
+
+var num1 = parseFloat(prompt("Ingrese el primer numero: "));
+var num2 = parseFloat(prompt("Ingrese el segundo numero: "));
